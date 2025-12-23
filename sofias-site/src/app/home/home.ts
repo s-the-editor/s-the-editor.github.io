d7ede18aca85dbd017274f6fde3ba4aca466fd31
Sofia Gukelberger <sgraveswrites@gmail.com>
2025-12-22 20:05:40 -0500

    Visible menu

---
+++ sofias-site/src/app/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { Menu } from '../menu/menu';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    Menu
+
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
